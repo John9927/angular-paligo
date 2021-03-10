221f9ae60d6dd2fe1a9bee9088083813d2805842
Giovanni Izzi <giovanni9972@libero.it>
2021-03-10 11:09:40 +0100

    Add Sidebar & Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
